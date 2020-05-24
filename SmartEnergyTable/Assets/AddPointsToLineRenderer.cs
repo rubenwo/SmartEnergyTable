f11c0898856eb5787ac9068e971cb1496ac4d5bc
Bilel <bbghiel@avans.nl>
2020-05-24 20:32:35 +0200

    - Graph polish
    - Added camera control class, need commands now from server

--- SmartEnergyTable/Assets/AddPointsToLineRenderer.cs
+++ SmartEnergyTable/Assets/AddPointsToLineRenderer.cs
@@ -8,7 +8,7 @@ public class AddPointsToLineRenderer : MonoBehaviour
     public Color c1 = Color.yellow;
     public Color c2 = Color.red;
 
-    public Color TextColor = Color.blue;
+    public Color TextColor = Color.black;
 
     // Represents all our raw data
     private List<Vector3> _points = new List<Vector3>();
@@ -19,12 +19,6 @@ public class AddPointsToLineRenderer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.widthMultiplier = 1f;
-        lineRenderer.positionCount = _points.Count;
-        lineRenderer.transform.rotation = gameObject.transform.rotation;
-
         float relX, relY, relZ;
 
         RectTransform b = gameObject.GetComponent<RectTransform>();
@@ -36,7 +30,7 @@ public class AddPointsToLineRenderer : MonoBehaviour
 
         // Calculate Graph size
         float maxY = _values.Max(points => points);
-        float diffYPerX = b.rect.height / maxY;
+        float diffYPerX = b.rect.height / maxY * 0.9f;
         float diffX = b.rect.width / _values.Count;
 
         short counter = 0;
@@ -45,24 +39,32 @@ public class AddPointsToLineRenderer : MonoBehaviour
         {
             float startX = relX + counter * diffX;
             float endX = relX + counter * diffX + diffX;
+            float upperY = relY + val * diffYPerX;
 
             for (float c = startX; c < endX; c++)
             {
                 // Draw graph
                 _points.Add(new Vector3(c, relY, relZ));
-                _points.Add(new Vector3(c, relY + val * diffYPerX, relZ));
-                _points.Add(new Vector3(c+1, relY + val * diffYPerX, relZ));
-                _points.Add(new Vector3(c+1, relY, relZ));
+                _points.Add(new Vector3(c, upperY, relZ));
+                _points.Add(new Vector3(c + 1, upperY, relZ));
+                _points.Add(new Vector3(c + 1, relY, relZ));
             }
 
-            AddText(val.ToString(), new Vector3(relX + counter * diffX, relY + val * diffYPerX, relZ),
-                                           new Vector3(relX + counter * diffX + diffX, relY + val * diffYPerX, relZ));
-            
-            // Draw info panel
+            //AddGraphBar(new Vector3(startX, relY, relZ), new Vector3(endX, upperY, relZ));
 
-            counter++;
+
+            //Add text above our graph bar here
+            AddText(val.ToString(), new Vector3(relX + counter * diffX, relY + val * diffYPerX + 10, relZ),
+                                           new Vector3(relX + counter * diffX + diffX, relY + val * diffYPerX + b.rect.height / 10, relZ));
+
+           counter++;
         }
 
+        LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 1f;
+        lineRenderer.positionCount = _points.Count;
+        //lineRenderer.transform.rotation = gameObject.transform.rotation;
 
 
         // A simple 2 color gradient with a fixed alpha of 1.0f.
@@ -77,6 +79,37 @@ public class AddPointsToLineRenderer : MonoBehaviour
 
     }
 
+    //private void AddGraphBar(Vector3 leftBottom, Vector3 rightTop)
+    //{
+    //    // Create the Text GameObject.
+
+    //    GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+    //    // Position and rotation plane
+    //    //plane.transform.position = leftBottom;
+    //    var a = plane.AddComponent<RectTransform>();
+
+    //    //GameObject textGO = new GameObject("bars");
+    //    plane.transform.parent = GameObject.Find("Plane (1)").transform;
+    //    var mesh = plane.GetComponent<MeshRenderer>();
+    //    //mesh.material = new Material(Shader.Find("Specular"));
+    //    mesh.material.color = Color.blue;
+    //    //textMesh.mater
+    //    //textMesh.color = TextColor;
+    //    //textMesh.alignment = TextAlignment.Center;
+    //    //textMesh.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+
+    //    //var rTransf = (RectTransform)plane.transform;
+    //    a.transform.position = leftBottom;
+    //    a.transform.rotation = GameObject.Find("Plane (1)").transform.rotation;
+    //    //start.y += rTransf.rect.height / 10;
+    //    //textMesh.transform.position = start;
+    //    a.sizeDelta = new Vector2(rightTop.x - leftBottom.x, rightTop.y - leftBottom.y);
+    //    //textMesh.text = _names.ElementAt(_values.IndexOf(int.Parse(text))) + "\n " + text;
+        
+
+    //}
+
     private void AddText(string text, Vector3 start, Vector3 end)
     {
         // Create the Text GameObject.
@@ -90,9 +123,9 @@ public class AddPointsToLineRenderer : MonoBehaviour
 
         var rTransf = (RectTransform)gameObject.transform;
 
-        start.y += rTransf.rect.height/10;
+        //start.y += rTransf.rect.height/10;
         textMesh.transform.position = start;
-        ((RectTransform)gameObject.transform).sizeDelta = new Vector2(end.x - start.x, end.y - start.y);
+        rTransf.sizeDelta = new Vector2(Math.Abs(end.x - start.x), Math.Abs(end.y - start.y));
         textMesh.text = _names.ElementAt(_values.IndexOf(int.Parse(text))) + "\n "+ text;
 
         //label.transform.position = start;
