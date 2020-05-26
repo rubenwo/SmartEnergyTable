8c843556a018f561291bffe76c824de61ac952ce
Bilel <bbghiel@avans.nl>
2020-05-26 20:28:13 +0200

    -
    moves some stuff

--- SmartEnergyTable/Assets/AddPointsToLineRenderer.cs
+++ SmartEnergyTable/Assets/AddPointsToLineRenderer.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading;
 using TMPro;
@@ -17,33 +18,47 @@ public class AddPointsToLineRenderer : MonoBehaviour
 
     public Color TextColor = Color.black;
 
+    public enum GraphType { DAILY, MONTHLY };
+    public GraphType GraphTypeToDisplay = GraphType.DAILY;
+
     // Which Json value do you want to show? (Must exist else won't show)
     public string GraphPropertyName = "TotalDemand";
 
     // Represents all our raw data
     private List<Vector3> _points = new List<Vector3>();
 
-    private EnergyDemandHourly2[] _energy;
+    private EnergyDataContainer EnergyDataStore;
 
     IEnumerator GetHourly()
     {
-        UnityWebRequest www = UnityWebRequest.Get("http://localhost:8080/Hourly");
+        UnityWebRequest www = UnityWebRequest.Get("https://smartenergytable.rubenwoldhuis.nl/energydata");
+        www.certificateHandler = null;
+
         yield return www.Send();
 
-        if (www.isError)
+        if (www.isNetworkError)
         {
             Debug.Log(www.error);
         }
         else
         {
-            _energy = JsonConvert.DeserializeObject<EnergyDemandHourly2[]>(www.downloadHandler.text);
 
-            //var hourlyData = UnityWebRequest.Get("http://localhost:8080/Hourly");
-            //var monthlyData = UnityWebRequest.Get("http://localhost:8080/Monthly");
+            EnergyDataStore = JsonConvert.DeserializeObject<EnergyDataContainer>(www.downloadHandler.text);
+            Debug.Log(EnergyDataStore.EnergyUser[0].Name);
+            // First entries are invalid and cause errors.
+            EnergyDataStore.removeWrongEntries();
+            EnergyDataStore.limitBy(10);
+
+            dynamic data = new List<object>();
+
+            switch (GraphTypeToDisplay)
+            {
+                case GraphType.DAILY: data = EnergyDataStore.EnergyUser; break;
+                case GraphType.MONTHLY: data = EnergyDataStore.EnergyDemand; break;
+            }
 
             // Set Title bar
             GameObject.Find("TitleBar").GetComponent<TextMeshPro>().text = GraphPropertyName;
-
 
             float relX, relY, relZ;
 
@@ -53,22 +68,38 @@ public class AddPointsToLineRenderer : MonoBehaviour
             relY = gameObject.transform.position.y - b.rect.height / 2;
             relZ = 50;
 
-            // Calculate Graph size
-            _energy = _energy.Take(10).Skip(1).ToArray();
-            int maxY = _energy.Max(eData => (int)Convert.ToDouble(eData.GetType().GetProperty(GraphPropertyName).GetValue(eData)));
+            int maxY = 0;
 
+            foreach (var a in data)
+            {
+                int num = (int)Double.Parse(a.GetType().GetProperty(GraphPropertyName).GetValue(a));
+                // Get value and see if it's higher. Then make it our new highest number, if higher.
+                Debug.Log(num);
+
+                if (num > maxY)
+                    maxY = num;
+            }
+
+            // Calculate desired sizes
             float diffYPerX = b.rect.height / maxY * 0.9f;
-            float diffX = b.rect.width / _energy.Length;
+            float diffX = b.rect.width / data.Count;
 
             short counter = 0;
             // Generate 4 points for each raw value
-            foreach (var values in _energy)
+            foreach (var values in data)
             {
                 var val = (float)Convert.ToDouble(values.GetType().GetProperty(GraphPropertyName).GetValue(values));
 
                 float startX = relX + counter * diffX;
                 float endX = relX + counter * diffX + diffX;
                 float upperY = relY + val * diffYPerX;
+
+                //Debug.Log("Start");
+                //Debug.Log(diffYPerX);
+                //Debug.Log(diffX);
+                //Debug.Log(startX);
+                //Debug.Log(endX);
+                //Debug.Log(upperY);
 
                 for (float c = startX; c < endX; c++)
                 {
@@ -118,6 +149,11 @@ public class AddPointsToLineRenderer : MonoBehaviour
         StartCoroutine(GetHourly());
     }
 
+    void calculateAverage(string unitName, string property)
+    {
+
+    }
+
     private void AddText(string text, string value, Vector3 start, Vector3 end)
     {
         // Create the Text GameObject.
@@ -146,8 +182,37 @@ public class AddPointsToLineRenderer : MonoBehaviour
 
 
 }
+// Class used to deserialize complex json array
+public class EnergyDataContainer
+{
+    public List<EnergyUser2> EnergyUser { get; set; }
+    public List<EnergyDemandHourly2> EnergyDemand { get; set; }
 
-class EnergyUser2
+    public EnergyDataContainer()
+    {
+        EnergyUser = new List<EnergyUser2>();
+        EnergyDemand = new List<EnergyDemandHourly2>();
+    }
+
+    public void removeWrongEntries()
+    {
+        //Debug.Log(EnergyUser.FindIndex(0, EnergyUser.Count, (user) => user.Name == "Laplace"));
+        EnergyUser = EnergyUser.Where(user => user.Name != "Name").ToList();
+        EnergyDemand = EnergyDemand.Where(user => user.Name != "Name").ToList();
+
+        limitBy(10);
+    }
+
+    internal void limitBy(int v)
+    {
+        EnergyUser = EnergyUser.Take(v).ToList();
+        EnergyDemand = EnergyDemand.Take(v).ToList();
+    }
+}
+
+// Classes are also loaded in gRPC but serializing to these classes seems to give our objects other variablenames for some reason
+// Quick-fix: Use 2 new classes and serialize them into these.
+public class EnergyUser2
 {
     public string Time { get; set; }
     public string Label { get; set; }
@@ -168,7 +233,7 @@ class EnergyUser2
     }
 }
 
-class EnergyDemandHourly2
+public class EnergyDemandHourly2
 {
 
     public string Id { get; set; }
