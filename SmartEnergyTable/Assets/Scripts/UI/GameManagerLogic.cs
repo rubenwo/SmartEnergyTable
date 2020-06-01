863117882a14e863b38be9e220a1035647a7660a
Bilel <bbghiel@avans.nl>
2020-06-01 15:12:05 +0200

    Removing files

--- SmartEnergyTable/Assets/Scripts/UI/GameManagerLogic.cs
+++ SmartEnergyTable/Assets/Scripts/UI/GameManagerLogic.cs
@@ -1,13 +1,18 @@
 ﻿using Network;
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManagerLogic : MonoBehaviour
 {
     private NetworkManager _netMan;
     private string _id = Guid.NewGuid().ToString();
+
+    public Button graphButton;
+
+    private bool _graphsActive;
 
     // Start is called before the first frame update
     void Start()
@@ -34,13 +39,13 @@ public class GameManagerLogic : MonoBehaviour
             
             // Cheats
             _netMan.AddToken("Windmill", 90, new Vector3(50, 0, 0), 2);
-
+            //_netMan.Add
         } catch
         {
 
         }
 
-
+        graphButton.onClick.AddListener(() => showGraphs());
 
     }
 
@@ -52,6 +57,48 @@ public class GameManagerLogic : MonoBehaviour
 
     void Destroy()
     {
+
+    }
+
+    public void showGraphs()
+    {
+        _graphsActive = !_graphsActive;
+
+
+        if (_graphsActive)
+        {
+            var data = _netMan.GetEnergyData();
+
+            foreach (var token in SceneManager.GetActiveScene().GetRootGameObjects().Where(ob => ob.name.Contains("Windmill") || ob.name.Contains("Solar Panel") || name.Contains("Battery")))
+            {
+                addGraphToScene((GameObject)token);
+            }
+        } else
+        {
+            foreach (var token in SceneManager.GetActiveScene().GetRootGameObjects().Where(ob => ob.name.Contains("GenGraph")))
+            {
+                token.Destroy();
+            }
+        }
+    }
+
+
+    void addGraphToScene(GameObject ob)
+    {
+        // Get our already existing graph
+        var graphCanvas = UnityEngine.Object.Instantiate(SceneManager.GetActiveScene().GetRootGameObjects().First(obj => obj.name.Contains("GraphCanvas")));
+
+        var obPos = ob.transform.position;
+        obPos.y += 10;
+        obPos.z += 10;
+
+        graphCanvas.name = "GenGraph" + ob.name;
+        graphCanvas.SetActive(true);
+
+        var graphScript = graphCanvas.GetComponent<AddPointsToLineRenderer>();
+        graphScript.GraphTypeToDisplay = AddPointsToLineRenderer.GraphType.POWER_UNIT;
+
+        graphCanvas.transform.parent = this.gameObject.transform;
 
     }
 
